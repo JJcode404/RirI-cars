d6f5590cef57b6340a909a362c9d84bd7ad247fc
JJcode404 <khamjapher1@gmail.com>
2026-07-04 20:15:45 +0300

    feat: update allowedHosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["6e11-102-215-190-137.ngrok-free.app"],
+    allowedHosts: ["7176-102-140-196-11.ngrok-free.app"],
   },
   preview: {
     allowedHosts: ["riri-cars.onrender.com"],
